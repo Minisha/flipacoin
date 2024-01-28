a0d254fc557f3f1e28b4866e4ae389e5cb6ae35c
minisha <minisha.mit@gmail.com>
2024-01-28 11:34:47 +0100

    Initial Commit

---
+++ flipacoin/flipacoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  flipacoinApp.swift
+//  flipacoin
+//
+//  Created by minisha murugan on 28/1/24.
+//
+
+import SwiftUI
+
+@main
+struct flipacoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
